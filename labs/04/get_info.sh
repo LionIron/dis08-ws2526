435d233f9248bd94337d7c0dd2e99503f6dff8f8
LionIron <lion_iron.sbaih@smail.th-koeln.de>
2025-11-25 12:24:59 +0100

    Finishing get_info.sh for all tasks

--- labs/04/get_info.sh
+++ labs/04/get_info.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DATA_DIR="shell-lesson/shell-lesson/data"
+DATA_DIR="shell-lesson"
 
 echo "Task 1:"
 head -n 3 "$DATA_DIR/2014-01_JA.tsv"
@@ -9,4 +9,4 @@ echo "Task 2:"
 wc -l "$DATA_DIR"/*.tsv
 
 echo "Task 3:"
-wc -l "$DATA_DIR"/*.tsv | sort -nr | head -n 1
+wc -l "$DATA_DIR"/*.tsv | grep -v "total" | sort -nr | head -n 1
